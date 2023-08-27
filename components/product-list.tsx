9589fa3ff0d6bd25759094d6add8cda11ca2e858
Rahdeg <walett95@gmail.com>
2023-08-27 18:33:47 +0100

    fix

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -27,7 +27,7 @@ const ProductList: React.FC<ProductListProps> = ({ title, data }) => {
                 {title}
             </h3>
             {products.length === 0 && <NoResults />}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {
                     products.map((item) => (
                         <ProductCard data={item} key={item.id} />
